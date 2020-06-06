57e68fe0dc926eff087d53f3ebf170be1bcab85c
mcfinley <mcfinley@yandex.ru>
2020-06-06 09:36:53 +0300

    Update the filterign config schema

--- example/App/index.js
+++ example/App/index.js
@@ -2,43 +2,59 @@ import React from 'react'
 
 import ReactAwesomeFilter from '../../lib'
 
-const FILTERS = [
+const GENERATORS = [
   (str) => {
     if (str.toLowerCase().indexOf('size') === -1) {
       return null
     }
 
     const num = +((str.match(/\d+/) || [])[0]) || 0
+    const op = str.indexOf('>') > -1 ? 'gt' : (str.indexOf('<') > -1 ? 'lt' : 'eq')
 
-    return {
-      value: { type: 'size-eq', num },
-      label: `Size eq ${num}`,
-    }
+    return { type: 'size', meta: { op: op, value: num } }
   },
-  (str) => {
-    return {
-      value: { type: 'search', str },
-      label: `Search for ${str}`,
-    }
-  },
+  (str) => ({ type: 'name', meta: str }),
 ]
+
+const HIGHLIHGTS = {
+  'size': ({ meta }) => `Size ${meta.op} ${meta.value}`,
+  'name': ({ meta }) => `Search for "${meta}" in name`,
+}
+
+const LABELS = {
+  'size': ({ meta }) => `Size ${meta.op} ${meta.value}`,
+  'name': ({ meta }) => `Search for "${meta}" in name`,
+}
+
+const QUERY  = {
+  'size': ({ meta }) => `size ${meta.op === 'gt' ? '>' : '<'} ${meta.value}`,
+  'name': ({ meta }) => `name LIKE "%${meta}%"`,
+}
 
 export default class App extends React.PureComponent {
   state = { value: [] }
   setValue = (value) => this.setState({ value })
 
+  getQuery = () => this.state.value.reduce((acc, item) => `${acc ? `${acc} AND ` : ''}${QUERY[item.type](item)}`, '')
+
   render () {
     const { value } = this.state
 
     return (
-      <div>
-        Basic Example:
-        <div style={{ padding: 20 }}>
+      <div style={{ padding: 30 }}>
+        <h1>React Awesome Filter</h1>
+        <p>Yet another react filtering component</p>
+        <div style={{ }}>
           <ReactAwesomeFilter
             value={value}
-            filters={FILTERS}
+            generators={GENERATORS}
+            highlights={HIGHLIHGTS}
+            labels={LABELS}
             onChange={this.setValue}
           />
+        </div>
+        <div>
+          Query: <code>SELECT * FROM table {this.getQuery() !== '' ? `WHERE ${this.getQuery()}` : ''};</code>
         </div>
       </div>
     )
